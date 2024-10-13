63a5df85359c893ce338e0f118270dffa9165204
Dan Alin <alin.ioan.dan@accenture.com>
2024-10-14 00:02:40 +0200

    chore: add keymaps

--- lua/alin-dan/core/keymaps.lua
+++ lua/alin-dan/core/keymaps.lua
@@ -3,6 +3,33 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true }
 
+-- save file
+keymap.set("n", "<leader>w", "<cmd>w<CR>", opts)
+
+-- quit file
+keymap.set("n", "<leader>q", "<cmd>q<CR>", opts)
+
+-- esc insert mode
+keymap.set("i", "jk", "<ESC>", opts)
+
+-- delete buffer
+keymap.set("n", "<leader>bd", "<cmd>bd<CR>", opts)
+
+-- delete other buffers
+keymap.set("n", "<leader>bo", "<cmd>BufOnly<CR>", opts)
+
+-- goto end of line
+keymap.set("n", "ge", "$", opts)
+
+-- goto start of line
+keymap.set("n", "gs", "^", opts)
+
+
+
+-- move between buffers
+keymap.set("n", "<TAB>", "<cmd>bn<CR>", opts)
+keymap.set("n", "<S-TAB>", "<cmd>bp<CR>", opts)
+
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
@@ -14,5 +41,5 @@ keymap.set("n", "-", "<C-x>", { desc = "Decrement number" }) -- decrement
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- move lines
-keymap.set("n", "<C-j>", ":m .+1<CR>==", { desc = "Move line down" })
-keymap.set("n", "<C-k>", ":m .-2<CR>==", { desc = "Move line up" })
+keymap.set("n", "<J>", ":m .+1<CR>==", { desc = "Move line down" })
+keymap.set("n", "<K>", ":m .-2<CR>==", { desc = "Move line up" })
