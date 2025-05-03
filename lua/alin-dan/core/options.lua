60e2aaa5b3bef6e436512f66ef495cfebdf67238
alin <danalin06@gmail.com>
2025-05-04 00:12:35 +0200

    chore: change option

--- lua/alin-dan/core/options.lua
+++ lua/alin-dan/core/options.lua
@@ -31,9 +31,6 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
--- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
