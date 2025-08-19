637ba30f66363846339685994447f4865cfcffbf
alin <danalin06@gmail.com>
2025-08-20 01:00:36 +0300

    fix: alpha dashboard scroll

--- lua/alin-dan/plugins/alpha.lua
+++ lua/alin-dan/plugins/alpha.lua
@@ -26,20 +26,54 @@ return {
 			dashboard.button("q", "   Quit NVIM", "<cmd>qa<CR>"),
 		}
 
-		-- Add padding to center vertically
+		-- Simple layout without excessive padding to prevent scrolling
 		dashboard.config.layout = {
-			{ type = "padding", val = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }) },
+			{ type = "padding", val = 2 },
 			dashboard.section.header,
-			{ type = "padding", val = 5 },
+			{ type = "padding", val = 2 },
 			dashboard.section.buttons,
-			{ type = "padding", val = 3 },
+			{ type = "padding", val = 1 },
 			dashboard.section.footer,
 		}
 
 		-- Send config to alpha
 		alpha.setup(dashboard.config)
 
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+		-- Create autocommand group for alpha buffer settings
+		local alpha_group = vim.api.nvim_create_augroup("alpha_buffer_settings", { clear = true })
+		
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "alpha",
+			group = alpha_group,
+			callback = function()
+				local opts = { silent = true, buffer = true }
+				-- Disable all scrolling
+				vim.keymap.set('n', '<ScrollWheelUp>', '<Nop>', opts)
+				vim.keymap.set('n', '<ScrollWheelDown>', '<Nop>', opts)
+				vim.keymap.set('n', '<C-u>', '<Nop>', opts)
+				vim.keymap.set('n', '<C-d>', '<Nop>', opts)
+				vim.keymap.set('n', '<C-b>', '<Nop>', opts)
+				vim.keymap.set('n', '<C-f>', '<Nop>', opts)
+				vim.keymap.set('n', '<PageUp>', '<Nop>', opts)
+				vim.keymap.set('n', '<PageDown>', '<Nop>', opts)
+				vim.keymap.set('n', 'gg', '<Nop>', opts)
+				vim.keymap.set('n', 'G', '<Nop>', opts)
+				
+				-- Set buffer options
+				vim.opt_local.foldenable = false
+				vim.opt_local.modifiable = false
+				vim.opt_local.swapfile = false
+				vim.opt_local.bufhidden = 'wipe'
+				vim.opt_local.buftype = 'nofile'
+				vim.opt_local.cursorline = false
+				vim.opt_local.list = false
+				vim.opt_local.number = false
+				vim.opt_local.relativenumber = false
+				vim.opt_local.scrollbind = false
+				vim.opt_local.signcolumn = 'no'
+				vim.opt_local.wrap = false
+				vim.opt_local.scrolloff = 0
+			end
+		})
 	end,
 }
